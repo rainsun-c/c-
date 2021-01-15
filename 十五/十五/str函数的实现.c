2b944f37cd5089d572527e19aa2e119078211e6f
sunrain-c <2013m3y8r9>
2021-01-15 10:54:53 +0800

    str函数的实现与回调函数的应用

---
+++ 十五/十五/str函数的实现.c
@@ -0,0 +1,180 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#if 0
+//1，自己实现strchr和strstr
+char* my_strchr(char*p, char p1)
+{
+	assert(p != NULL);
+	assert(p1 != NULL);
+	while (*p!='\0')
+	{
+		if (*p == p1)
+		{
+			return p;
+		}
+		p++;
+	}
+	return (char*)-1;
+}
+
+char* my_strstr(char* p,char* p1)
+{
+	assert(p != NULL);
+	assert(p1 != NULL);
+	int count = 0;
+	int size = 0;
+	for ( ; *p1 != '\0'; p1++)
+	{
+		size++;
+	}
+	while (*p!= '\0'&&*p1!= '\0')
+	{
+		if (*p == *p1)
+		{
+			count++;
+		}
+		else
+		{
+			count = 0;
+		}
+		if (count == size)
+		{
+			return p+size;
+		}
+		p++;
+		p1++;
+	}
+	return (char*)-1;
+}
+int main11()
+{
+	char str1[] = "bakbvvnd";
+	char str2[] = "ek";
+
+	char* t=my_strstr(str1,str2);
+	printf("%s\n", strstr(str1,str2));
+
+	char* p=my_strchr(str1,'b');
+	printf("%s", p);
+	while (1);
+	return 0;
+}
+#endif
+
+#if 0
+//比较strcmp
+int my_strcmp(char* p,char* p1)
+{
+	assert(p != NULL);
+	assert(p1 != NULL);
+	while (*p != '\0'&&*p1 != '\0')
+	{
+		if (*p > *p1)
+		{
+			return 1;
+		}
+		if (*p < *p1)
+		{
+			return -1;
+		}
+		if (*p==*p1)
+		{
+			p++;
+			p1++;
+		}
+	}
+	return 0;
+}
+
+int main()
+{
+	char str[50] = "bsuksca";
+	char str1[] = "sgvs";
+	int temp = my_strcmp(str, str1);
+	printf("%d", temp);
+	/*int t=strcmp(str,str1);
+	printf("%d",t);*/
+	while (1);
+	return 0;
+}
+#endif
+
+#if 0
+//字符串长度
+int my_strlen(char* p)
+{
+	int count = 0;
+	assert(p!=NULL);
+	while (*p++!='\0')
+	{
+		count++;
+	}
+	return count;
+}
+int main()
+{
+	char str[] = "dsiuhas";
+	int temp = my_strlen(str);
+	printf("%d",temp);
+	while (1);
+	return 0;
+}
+#endif
+
+#if 0
+//字符串复制
+char* my_strcpy(char* p, char* p1)
+{
+	char* t = p;
+	assert(p != NULL);
+	assert(p1 != NULL);
+	while (*p1!='\0')
+	{
+		*p++=*p1++;
+	}
+	*p = '\0';
+	return t;
+}
+int main()
+{
+	char str1[30] = "abcdest";
+	char str2[23] = "qwer";
+	char* temp=my_strcpy(str1,str2);
+	printf("%s",temp);
+	while (1);
+	return 0;
+}
+#endif
+
+#if 0
+//字符串拼接
+char* my_strcat(char* p,char* p1)
+{
+	char* temp = p;
+	assert(p != NULL);
+	assert(p1 != NULL);
+	while (*p != '\0')
+	{
+		p++;
+	}
+	while (*p1)
+	{
+		*p = *p1;
+		p++;
+		p1++;
+	}
+	*p = '\0';
+	return temp;
+}
+int main()
+{
+	char str1[20] = "fsgsew";
+	char str2[] = "nbdal";
+	my_strcat(str1, str2);
+	puts(str1);
+	while (1);
+	return 0;
+}
+#endif
---
+++ 十五/十五/回调函数.c
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+typedef double data;
+typedef data(*p1)(data a, data b);
+
+#if 0
+//有两个10个元素的数组,分别为A和B,编程实现相同下标B的元素小于A的元素的交换:(使用回调函数实现)
+void change(int* a,int* b)
+{
+	int temp = 0;
+	if (*b < *a)
+	{
+		temp = *b;
+		*b = *a;
+		*a= temp;
+	}
+}
+void all(int* p, int*c, void (*change)(int*, int*))
+{
+	for (int i = 0; i < 10; i++)
+	{
+		change(p, c);
+		p++;
+		c++;
+	}
+}
+int main()
+{
+	int a[10] = {4,3,1,7,5,9,220,12,47,66};//b<a则交换
+	int b[10] = {6,1,4,8,12,34,5,75,324,2};
+	all(a, b, change);
+	for (int i = 0; i < 10; i++)
+	{
+		printf("%3d ",a[i]);
+	}
+	printf("\n");
+	for (int i = 0; i < 10; i++)
+	{
+		printf("%3d ", b[i]);
+	}
+	while (1);
+	return 0;
+}
+#endif
+
+#if 0
+//简单计算器的实现（回调函数）
+data add(data a, data b)
+{
+	return (a + b);
+}
+data sub(data a, data b)
+{
+	return (a - b);
+}
+data ride(data a, data b)
+{
+	return (a*b);
+}
+data divs(data a, data b)
+{
+
+	return (a / b);
+}
+data mod(data a, data b)
+{
+	return ((int)a % (int)b);
+}
+data calculator(char c, data a, data b,data(*p)(data a, data b))
+{
+	switch (c)
+	{
+	case '+':
+		p = add;
+		break;
+	case '-':
+		p = sub;
+		break;
+	case '*':
+		p = ride;
+		break;
+	case '/':
+		p = divs;
+		break;
+	case '%':
+		p = mod;
+		break;
+	default:
+		break;
+	}
+	if (p == NULL)
+		return -1;
+	
+	return p(a, b);
+}
+int main()
+{
+	int a = 0;
+	int b = 0;
+	char c = 0;
+	p1 p=NULL;
+	puts("请输入：");
+	scanf("%d %c %d",&a,&c,&b);
+	data t=calculator(c,a,b,p);
+	printf("%lf", t);
+	while (1);
+	return 0;
+}
+#endif
